e61a7ea8ea70fa8a5250b3c26cdc5675bc2f3bd5
astellarius <zak.lake0@gmail.com>
2022-07-16 23:04:53 -0700

    complete bellman ford alg

--- graph_algorithms/bellman_ford.py
+++ graph_algorithms/bellman_ford.py
@@ -1,6 +1,34 @@
+
+
+from numpy import Infinity
+from sqlalchemy import true
+
+PARENT = 0
+SHORTEST_PATH_WEIGHT = 1 # estimate shortest path weight
+
+def relax(edge, vertex_info):
+    from_node, to_node, edge_weight = edge
+    if vertex_info[from_node][SHORTEST_PATH_WEIGHT] + edge_weight < vertex_info[to_node][SHORTEST_PATH_WEIGHT]:
+        vertex_info[to_node][PARENT] = from_node
+        vertex_info[to_node][SHORTEST_PATH_WEIGHT] = vertex_info[from_node][SHORTEST_PATH_WEIGHT] + edge_weight
+        return True
+    return False
+
+def bellman_ford(edge_list, vertex_info):
+    vertices = len(vertex_info)
+    for i in range(0, vertices - 1):
+        for edge in edge_list:
+            relax(edge, vertex_info)
+
+    for edge in edge_list:
+        if relax(edge, vertex_info):
+            return False 
+    return True 
+        
+
 def main():
     edge_list = [
-        # [to, from, weight]
+        # [from, to, weight]
         ["s", "t", 6],
         ["s", "y", 7],
         ["t", "x", 5], 
@@ -13,7 +41,18 @@ def main():
         ["z", "s", 2], 
     ]
 
-    print(edge_list)
+    vertex_info = dict()
+
+    for edge in edge_list:
+        vertex_info[edge[0]] = [None, Infinity] # parent, estimate shortest path weight
+
+    source = "s"
+    vertex_info[source] = [None, 0]
+
+    if bellman_ford(edge_list, vertex_info):
+        print(vertex_info)
+    else:
+        print("The Graph has a negative cycle")
 
 
 main()
